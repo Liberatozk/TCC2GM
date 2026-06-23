b8a228969c246b06afd938f953ff04041589182a
Liberatozk <rm99738@estudante.fieb.edu.br>
2026-06-23 11:58:21 +0000

    Criação da api backend com spring boot

---
+++ eletrorecicla/src/main/java/com/itb/inf2gm/tcc/eletrorecicla/EletroreciclaApplication.java
@@ -0,0 +1,13 @@
+package com.itb.inf2gm.tcc.eletrorecicla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletroreciclaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletroreciclaApplication.class, args);
+	}
+
+}
---
+++ eletrorecicla/src/test/java/com/itb/inf2gm/tcc/eletrorecicla/EletroreciclaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.inf2gm.tcc.eletrorecicla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EletroreciclaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
